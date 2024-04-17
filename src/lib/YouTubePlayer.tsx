636a0df3109729bb07330e877f9de843b43d036f
Kurt Lekanger <kurt@lekanger.no>
2024-04-17 17:41:57 +0200

    style: add some padding to YouTube player

--- src/lib/YouTubePlayer.tsx
+++ src/lib/YouTubePlayer.tsx
@@ -5,7 +5,7 @@ import ReactPlayer from "react-player";
 
 export default function YouTubePlayer({ url }: { url: string }) {
 	return (
-		<div className="overflow-hidden">
+		<div className="overflow-hidden pt-2 pb-4">
 			<ReactPlayer url={url} style={{ maxWidth: "100%" }} />
 		</div>
 	);
