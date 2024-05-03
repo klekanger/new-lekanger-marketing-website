5279aeec3cb2d35b3ccc2b03fde5a27975c947a0
Kurt Lekanger <kurt@lekanger.no>
2024-05-03 15:29:36 +0200

    chore: remove sentry replays, not using those

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,15 +5,16 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://726c63624cc14675ada692eade7cb188@o849093.ingest.us.sentry.io/5816155",
+	dsn: "https://726c63624cc14675ada692eade7cb188@o849093.ingest.us.sentry.io/5816155",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+	// Setting this option to true will print useful information to the console while you're setting up Sentry.
+	debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+	// NOT USING REPLAYS
+	/* replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
@@ -26,5 +27,5 @@ Sentry.init({
       maskAllText: true,
       blockAllMedia: true,
     }),
-  ],
+  ], */
 });
